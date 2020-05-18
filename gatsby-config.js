fd7996f28b84e245eb35628d160272854a40204c
Willie Payne <williepain@gmail.com>
2020-05-18 19:05:38 -0400

    First prototype version of 2020 webpage

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,51 +1,52 @@
 module.exports = {
-  siteMetadata: {
-    siteTitle: `@rocketseat/gatsby-theme-docs`,
-    defaultTitle: `@rocketseat/gatsby-theme-docs`,
-    siteTitleShort: `gatsby-theme-docs`,
-    siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
-    siteUrl: `https://rocketdocs.netlify.com`,
-    siteAuthor: `@rocketseat`,
-    siteImage: `/banner.png`,
-    siteLanguage: `en`,
-    themeColor: `#7159c1`,
-    basePath: `/`,
-    footer: `Theme by Rocketseat`,
-  },
-  plugins: [
-    {
-      resolve: `@rocketseat/gatsby-theme-docs`,
-      options: {
-        configPath: `src/config`,
-        docsPath: `src/docs`,
-        githubUrl: `https://github.com/rocketseat/gatsby-themes`,
-        baseDir: `examples/gatsby-theme-docs`,
-      },
+    pathPrefix: "/Summer20",
+    siteMetadata: {
+        siteTitle: `DAB! Summer 2020`,
+        defaultTitle: `DAB! Summer 2020`,
+        siteTitleShort: `DAB! Summer 2020`,
+        siteDescription: `Static Web App for DAB 2020`,
+        siteUrl: `https://williepayne.com`,
+        siteAuthor: `@huriphoonado`,
+        siteImage: `/banner.png`,
+        siteLanguage: `en`,
+        themeColor: `#7159c1`,
+        basePath: `/`,
+        footer: `AMPED Research | STEM From Dance`,
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Rocketseat Gatsby Themes`,
-        short_name: `RS Gatsby Themes`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        display: `standalone`,
-        icon: `static/favicon.png`,
-      },
-    },
-    `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // trackingId: ``,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-canonical-urls`,
-      options: {
-        siteUrl: `https://rocketdocs.netlify.com`,
-      },
-    },
-    `gatsby-plugin-offline`,
-  ],
+    plugins: [
+        {
+            resolve: `@rocketseat/gatsby-theme-docs`,
+            options: {
+                configPath: `src/config`,
+                docsPath: `src/docs`,
+                githubUrl: `https://github.com/rocketseat/gatsby-themes`,
+                baseDir: `examples/gatsby-theme-docs`,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Rocketseat Gatsby Themes`,
+                short_name: `RS Gatsby Themes`,
+                start_url: `/`,
+                background_color: `#ffffff`,
+                display: `standalone`,
+                icon: `static/favicon.png`,
+            },
+        },
+        `gatsby-plugin-sitemap`,
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                // trackingId: ``,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-canonical-urls`,
+            options: {
+                siteUrl: `https://rocketdocs.netlify.com`,
+            },
+        },
+        `gatsby-plugin-offline`,
+    ],
 };
---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+import ReactRough, { Rectangle, LinearPath, Circle } from 'react-rough';
+
+const Container = styled.div`
+  width: 160px;
+  height: 90px;
+  background-size: contain;
+`;
+
+export default function Logo({}) {
+    return (
+        <Container>
+        <ReactRough height={90} width={160} renderer="svg">
+            <Rectangle height={50} width={100} strokeWidth={1} fill="grey" fillStyle="solid" stroke="black" x={15} y={25}  />
+            <LinearPath points={[ [13, 0], [30, 83], [61, 63] ]} strokeWidth={1}/>
+            <LinearPath points={[ [65, 30], [0, 54], [81, 67] ]} strokeWidth={1}/>
+            <Circle diameter={27} x={33} y={26} strokeWidth={1} fill="purple" fillStyle="solid"/>
+        </ReactRough>
+        </Container>
+    )
+}
+
+//import logo from '../assets/logo.svg';
